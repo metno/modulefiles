2a9ba2e27219a015cedc18eca00f512bdccb5522
Alvaro Valdebenito <alvarov@met.no>
2026-04-10 12:28:07 +0200

    kelvin/fahrenheit home

--- cams-reg/202511.lua
+++ cams-reg/202511.lua
@@ -12,8 +12,19 @@ More information
 
 whatis([==[Description: EMEP-MSC/W setup for CAMS2_40 forecasts.]==])
 
+function starts_with(str, sub)
+    return string.sub(str, 1, string.len(sub)) == sub
+end
+function home()
+    local host, home = os.getenv("HOSTNAME"), os.getenv("HOME")
+    if     starts_with(host, "kelvin")     then home = "/home/met_lf"
+    elseif starts_with(host, "fahrenheit") then home = "/home/sm_alvva"
+    end
+    return home
+end
+
 local version = "CAMS2-40."..myModuleVersion()
-local cwf = pathJoin(os.getenv("HOME"), "emep", "cwf")
+local cwf = pathJoin(home(), "emep", "cwf")
 local apps = pathJoin(cwf, "modules")
 local root = pathJoin(apps, version)
 
