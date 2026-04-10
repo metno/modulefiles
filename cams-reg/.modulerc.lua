49093e6ba6c838f7a677d6f9212abed1108338b9
Alvaro Valdebenito <alvarov@met.no>
2026-04-10 11:39:34 +0200

    cams-reg/u10b

--- cams-reg/.modulerc.lua
+++ cams-reg/.modulerc.lua
@@ -1,4 +1,6 @@
 module_version("cams-reg/202511", "u10")
 module_version("cams-reg/202511", "ira2026")
+module_version("cams-reg/202605", "u10b")
 module_alias("cams2-40/u10",     "cams-reg/202511")
 module_alias("cams2-40/ira2026", "cams-reg/202511")
+module_alias("cams2-40/u10b",    "cams-reg/202605")
---
+++ cams-reg/202605.lua
@@ -0,0 +1,29 @@
+help([==[
+
+Description
+===========
+EMEP-MSC/W setup for CAMS Regional Air Quality (CAMS2_40)
+
+
+More information
+================
+ - Homepage: https://atmosphere.copernicus.eu/european-air-quality-forecast-plots/
+]==])
+
+whatis([==[Description: EMEP-MSC/W setup for CAMS2_40 forecasts.]==])
+
+local version = "CAMS2-40."..myModuleVersion()
+local cwf = pathJoin(os.getenv("HOME"), "emep", "cwf")
+local apps = pathJoin(cwf, "modules")
+local root = pathJoin(apps, version)
+
+setenv("EMEP_PROJECT", myModuleName())
+setenv("EMEP_MODULES", myModuleFullName())
+setenv("EMEP_VERSION", version)
+setenv("EMEP_DATA",    pathJoin(root, "data"))
+setenv("EMEP_DATA_ROOT",pathJoin(cwf, "Data"))
+
+--purge()
+prepend_path("PATH",      pathJoin(root, "bin"))
+prepend_path("PYTHONPATH",pathJoin(apps, "python"))
+load("Python/3.12.3-env-hpc1-gcc-2024a-eb")
