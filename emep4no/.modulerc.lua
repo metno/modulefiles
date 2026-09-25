fcbfeb53aefd15e33ce0385e8f7229926f25b6b7
Alvaro Valdebenito <alvarov@met.no>
2026-09-25 13:29:34 +0200

    emep4no/u11

--- emep4no/.modulerc.lua
+++ emep4no/.modulerc.lua
@@ -1,1 +1,2 @@
 module_version("emep4no/202511", "u10")
+module_version("emep4no/202610", "u11")
---
+++ emep4no/202610.lua
@@ -0,0 +1,40 @@
+help([==[
+
+Description
+===========
+EMEP-MSC/W setup for Norwegian Air Quality
+
+
+More information
+================
+ - Homepage: https://luftkvalitet.miljodirektoratet.no/
+]==])
+
+whatis([==[Description: EMEP-MSC/W setup for Norwegian AQ forecasts.]==])
+
+function starts_with(str, sub)
+    return string.sub(str, 1, string.len(sub)) == sub
+end
+function home()
+    local host, home = os.getenv("HOSTNAME"), os.getenv("HOME")
+    if     starts_with(host, "kelvin")     then home = "/home/met_lf"
+    elseif starts_with(host, "fahrenheit") then home = "/home/sm_alvva"
+    end
+    return home
+end
+
+local version = "EMEP4NO."..myModuleVersion()
+local cwf = pathJoin(home(), "emep", "cwf")
+local apps = pathJoin(cwf, "modules")
+local root = pathJoin(apps, version)
+
+setenv("EMEP_PROJECT", myModuleName())
+setenv("EMEP_MODULES", myModuleFullName())
+setenv("EMEP_VERSION", version)
+setenv("EMEP_DATA",    pathJoin(root, "data"))
+setenv("EMEP_DATA_ROOT",pathJoin(cwf, "Data"))
+
+--purge()
+prepend_path("PATH",      pathJoin(root, "bin"))
+prepend_path("PYTHONPATH",pathJoin(apps, "python"))
+load("Python/3.12.3-env-hpc1-gcc-2024a-eb")
